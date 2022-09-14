175c2b32a5b5a8c7b356a2cfd9d24813a1753a13
JulioSalazar12 <u202017572@upc.edu.pe>
2022-09-14 05:49:54 -0500

    feat: tabla con datos de la api

--- src/components/DataTableView.vue
+++ src/components/DataTableView.vue
@@ -4,7 +4,7 @@
 
     <v-card>
       <v-card-title>
-        Nutrition
+        Peliculas
         <v-spacer></v-spacer>
         <v-text-field
           v-model="search"
@@ -16,7 +16,7 @@
       </v-card-title>
       <v-data-table
         :headers="headers"
-        :items="desserts"
+        :items="filmsList"
         :search="search"
       ></v-data-table>
   </v-card>
@@ -25,105 +25,59 @@
 </template>
 
 <script>
+  import DataTableService from "@/components/DataTable.Service"
+
   export default {
     name: 'DataTableView',
     data: () => ({   
       search: '',
-        headers: [
-          {
-            text: 'Dessert (100g serving)',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' },
-        ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
-        ],   
+      headers: [
+        { text: 'Imagen', value: 'actions', sortable: false },
+        { text: 'Titulo', value: 'title' },
+        { text: 'Director', value: 'director' },
+        { text: 'Año', value: 'release_date' },
+        { text: 'Puntaje', value: 'rt_score' },
+      ],
+      filmsList: [],
+
     }),
+    created () {
+      this.getAllFilms()
+    },
+    methods: {
+      getAllFilms(){
+      DataTableService.getAll()
+      .then((response) => {
+        this.filmsList = response.data.map(this.displayFilm);
+        console.log("Data obtenida:", response.data)
+        console.log("List:", this.filmsList)
+      })
+      .catch(e => {
+        console.log(e);
+      })
+    },
+    displayFilm(sampleFilm){
+      return {
+        title: sampleFilm.title,
+        director: sampleFilm.director,        
+        release_date: sampleFilm.release_date,        
+        rt_score: sampleFilm.rt_score,
+        image: sampleFilm.image      
+      }
+    },
+
+      editItem (item) {
+        this.editedIndex = this.filmsList.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      }
+    }
   }
 </script>
